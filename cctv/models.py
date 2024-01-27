7f5683d499695e6ea79d25f24cf425ac293d89d8
Satyam Namdev <111422209+Spyrosigma@users.noreply.github.com>
2024-01-28 03:25:34 +0530

    Update models.py

--- cctv/models.py
+++ cctv/models.py
@@ -1,6 +1,10 @@
 # models.py
 from django.db import models
 from django.shortcuts import render
+from ultralytics import YOLO
+from PIL import Image
+import os
+import sys
 
 class CCTVImage(models.Model):
     image = models.ImageField(upload_to='cctv/images/')
@@ -15,6 +19,33 @@ class CCTVImage(models.Model):
 
     def process_image(self):
         # Save the processed image
-        self.processed_image.save('processed_image.jpg', self.image, save=False)
+        model = YOLO('best.pt')
+        img_count = 0
+        image_paths = []
+        image_directory = "E:\\Django\Weapon-Detection-Web-App\media\cctv\images" 
+        for filename in os.listdir(image_directory):
+            if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".jpeg") or filename.endswith(".png"):
+                image_paths.append(os.path.join(image_directory, filename))
+                img_count += 1
+
+        print(f'Total number of images in the folder {image_directory} : ',img_count)
+
+        count = 0
+        for i in image_paths:
+            results = model(i) 
+            count += 1
+            for r in results:
+                im_array = r.plot() 
+                im = Image.fromarray(im_array[..., ::-1])  
+                im.save(f'E:\\Django\Weapon-Detection-Web-App\media\cctv\processed_images\{count}.jpg')  
+                # processed_image_path = f'E:\\Django\Weapon-Detection-Web-App\media\cctv\processed_images\{count}.jpg'
+                # self.processed_image = processed_image_path       
+                # self.processed_image.save('processed_image.jpg', self.image, save=False)
+            
+            # Assign the processed image path to the processed_image field
+
+        #self.processed_image = im.save(f'E:\\Django\Weapon-Detection-Web-App\media\cctv\processed_images\{count}.jpg')
+
+
 
 
